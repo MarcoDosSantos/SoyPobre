ef30b372437833ece5bcafe5f8c7d1703f6dd164
Marco Dos Santos <mds14421@gmail.com>
2020-09-19 13:15:50 -0300

    Agregamos Alert Action con la opción de volver a ser pobre.

--- 01-SoyPobre/01-SoyPobre/ViewController.swift
+++ 01-SoyPobre/01-SoyPobre/ViewController.swift
@@ -29,6 +29,25 @@ class ViewController: UIViewController {
         
         self.image.image = UIImage(named: "fullwallet")
         self.button.setTitle("Botón apretado", for: .normal)
+        
+        let alert = UIAlertController(title: "Ya sos rico.", message: "¿No es genial?", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "Ok", style: .default) { (_) in
+            print("Quieres ser rico")
+        }
+        
+        alert.addAction(action1)
+        
+        let action2 = UIAlertAction(title: "No", style: .destructive) { (_) in
+            print("No quiero ser rico")
+            self.label.text = "Soy Pobre"
+            
+            self.image.image = UIImage(named: "soypobre")
+            self.button.setTitle("Press Button", for: .normal)
+        }
+        
+        alert.addAction(action2)
+
+        self.show(alert, sender: nil)
     }
 }
 
